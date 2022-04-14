6077b1f1102d30f204ebbaabe97697ae5d01ef44
jazmc <jasmin.lumme@gmail.com>
2022-04-14 14:50:58 +0300

    env testing

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["inline-dotenv"],
+
+    plugins: [
+      [
+        "inline-dotenv",
+        {
+          path: "path/to/.env", // See motdotla/dotenv for more options
+        },
+      ],
+    ],
   };
 };
